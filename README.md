303450f872b2eab12896fbcec7b6af9f60c569ba
Abdurrahman eker <abdurrahmaneker58@gmail.com>
2019-10-13 13:20:21 +0300

    first commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Native Basic Timer
+
+## Install
+
+```
+yarn add react-native-basic-timer
+```
+
+## Usage
+
+```javascript
+<Timer/>
+```
+
+If you do not want the hours information to appear, you can use it as follows.
+
+```javascript
+<Timer hideHours/>
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+export default class Timer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            second: 0,
+        };
+
+        this._mounted = false;
+    }
+
+    componentDidMount() {
+        this._mounted = true;
+
+        var self = this;
+        this.interval = setInterval(() => {
+            if (!self._mounted)
+                return clearInterval(self.interval);
+
+            self.state.second++;
+            self.setState({
+                second: self.state.second,
+            });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        this._mounted = false;
+        clearInterval(this.interval);
+    }
+
+    zeroPrefix(number) {
+        return number / 10 >= 1 ? number : ('0' + number);
+    }
+
+    render() {
+        const hideHours = this.props.hideHours;
+        const second = this.state.second;
+        const hours = parseInt(second / 60 / 60);
+        const minutes = parseInt(second / 60) % 60;
+        const seconds = parseInt(second % 60);
+
+        return (
+            <Text>
+                {!hideHours && (hours + ':')}
+                {this.zeroPrefix(minutes) + ':'}
+                {this.zeroPrefix(seconds)}
+            </Text>
+        );
+    }
+}
